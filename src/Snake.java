e07832bf8af5c4ca579c1a449f40546d216d07ea
Matthew Greenhouse <m.greenhouse@hotmail.com>
2026-02-20 15:43:22 +0000

    First commit

---
+++ src/Snake.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Snake {
+
+    public static void main(String[] args) {
+        startGUI();
+        System.out.print("Hey bb");
+    }
+
+    public static void startGUI() {
+        // Start GUI
+
+    }
+}
