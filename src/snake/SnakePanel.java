7fceb84546bc0e3d255f84bca0060aebeed1da20
Matthew Greenhouse <m.greenhouse@hotmail.com>
2026-02-21 00:41:54 +0000

    Moving green blob

--- src/snake/SnakePanel.java
+++ src/snake/SnakePanel.java
@@ -2,19 +2,67 @@ package snake;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Toolkit;
+import java.awt.event.KeyListener;
 
 public class SnakePanel extends JPanel {
 
     private int snakeLength;
+    private int currentX;
+    private int currentY;
+    private boolean activeGame;
+
+    private Timer timer;
+
+    final int SQUARE_LENGTH = 30;
+
+    private int screenWidth;
+    private int screenHeight;
 
     public SnakePanel() {
+        // Toolkit retrieves system information
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenDimensions = toolkit.getScreenSize();
+        this.screenWidth = screenDimensions.width;
+        this.screenHeight = screenDimensions.height;
+
+        // Create the stage
+        setupBackground();
+
+        // Setup snake
         this.snakeLength = 3;
-        JPanel panel = new JPanel();
-        setupSnake(panel);
-        panel.setBackground(Color.green);
+        this.currentX = screenWidth/4;
+        this.currentY = screenHeight/2;
+
+        // Setting up the listeners
+        setupListeners();
+
+        timer = new Timer(50, e -> {
+            updateSnake();
+            repaint();
+        });
+
+        timer.start();
     }
 
-    protected void setupSnake(JPanel panel) {
-        panel.add(new JLabel("Hey " + snakeLength));
+    protected void setupBackground() {
+        setBackground(Color.BLACK);
+        setBorder(BorderFactory.createLineBorder(new Color(80, 80, 80), 30));
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.GREEN);
+        g2.fillRect(currentX, currentY, SQUARE_LENGTH, SQUARE_LENGTH);
+    }
+
+    protected void updateSnake() {
+        currentX += 30;
+    }
+
+    protected void setupListeners() {
+        updateSnake();
     }
 }
--- src/snake/SnakeWindow.java
+++ src/snake/SnakeWindow.java
@@ -1,13 +1,15 @@
 package snake;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.Color;
 
 public class SnakeWindow extends JFrame {
 
     public SnakeWindow(SnakePanel snakePanel) {
         // Create Snake Window
         setTitle("Snake Game");
-        add(snakePanel);
+        add(snakePanel, BorderLayout.CENTER);
 
         // Maximise JFrame to fit entire screen
         setExtendedState(MAXIMIZED_BOTH);
